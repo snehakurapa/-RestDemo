9510fc2eec5dd3dc1a0a9e38d75a9ac6fa4a6b60
sneha kurapati <kurapati0894@gmail.com>
2020-06-17 22:13:44 -0600

    Initial push

---
+++ src/main/java/com/frndsabba/demorest/Alien.java
@@ -0,0 +1,19 @@
+package com.frndsabba.demorest;
+
+public class Alien {
+	private String name;
+	private int points;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+}
